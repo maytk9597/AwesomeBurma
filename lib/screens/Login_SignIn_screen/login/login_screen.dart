02942cd5ad24208b5bfcb61a2a07b0d74626b925
May Thin Khaing <maythinkhaingminoz@gmail.com>
2021-06-21 16:11:50 +0630

    Login & Sign Up complete

--- lib/screens/Login_SignIn_screen/login/login_screen.dart
+++ lib/screens/Login_SignIn_screen/login/login_screen.dart
@@ -13,6 +13,8 @@ class LoginScreen extends StatelessWidget {
   String password;
   String email;
   bool validate;
+  bool loginFlag;
+  final _auth = FirebaseAuth.instance;
 
   void validateAndSave() {
     final FormState form = _formKey.currentState;
@@ -46,138 +48,173 @@ class LoginScreen extends StatelessWidget {
           ),
         ),
       ),
-      body: Form(
-        key: _formKey,
-        child: Container(
-          height: MediaQuery.of(context).size.height,
-          width: double.infinity,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    Column(
-                      children: <Widget>[
-                        FadeAnimation(
-                            1,
-                            Text(
-                              "Login",
-                              style: TextStyle(
-                                  color: ktextColor,
-                                  fontSize: 30,
-                                  fontWeight: FontWeight.bold),
-                            )),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        FadeAnimation(
-                            1.2,
-                            Text(
-                              "Login to your account",
-                              style: TextStyle(fontSize: 15, color: ktextColor),
-                            )),
-                      ],
-                    ),
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 40),
-                      child: Column(
+      body: Builder(
+        builder:(context)=>  Form(
+          key: _formKey,
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            width: double.infinity,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Column(
                         children: <Widget>[
                           FadeAnimation(
-                              1.2,
-                              makeInput(
-                                label: "Email",
-                                obscureText: false,
-                                onChanged: (text) {
-                                  email = text;
-
-                                  // print("Email = " + text);
-                                },
-                              )),
-                          FadeAnimation(
-                              1.3,
-                              makeInput(
-                                label: "Password",
-                                obscureText: true,
-                                onChanged: (text) {
-                                  password = text;
-                                  // print("Password = " + text);
-                                },
-                              )),
-                        ],
-                      ),
-                    ),
-                    FadeAnimation(
-                        1.4,
-                        Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 40),
-                          child: Container(
-                            padding: EdgeInsets.only(top: 3, left: 3),
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(50),
-                                border: Border.all(color: ktextColor)),
-                            child: MaterialButton(
-                              minWidth: double.infinity,
-                              height: getProportionateScreenHeight(70, context),
-                              onPressed: () {
-                                validateAndSave();
-                                if (validate == false) {
-                                  return null;
-                                } else {
-                                  User currentUser =
-                                      FirebaseAuth.instance.currentUser;
-                                  if (currentUser != null) {
-                                    Navigator.pushReplacement(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (context) =>
-                                                home_screen()));
-                                  }
-
-                                  print(
-                                      "Login ------------ > email $email Password $password");
-                                }
-                              },
-                              color: kMainColor,
-                              elevation: 0,
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(50)),
-                              child: Text(
+                              1,
+                              Text(
                                 "Login",
                                 style: TextStyle(
                                     color: ktextColor,
-                                    fontWeight: FontWeight.w600,
-                                    fontSize: 18),
-                              ),
-                            ),
+                                    fontSize: 30,
+                                    fontWeight: FontWeight.bold),
+                              )),
+                          SizedBox(
+                            height: 20,
                           ),
-                        )),
-                    FadeAnimation(
-                        1.5,
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
+                          FadeAnimation(
+                              1.2,
+                              Text(
+                                "Login to your account",
+                                style: TextStyle(fontSize: 15, color: ktextColor),
+                              )),
+                        ],
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 40),
+                        child: Column(
                           children: <Widget>[
-                            Text("Don't have an account?"),
-                            GestureDetector(
-                              onTap: () {
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) => SignUpScreen()));
-                              },
-                              child: Text(
-                                "Sign up",
-                                style: TextStyle(
-                                    fontWeight: FontWeight.w600, fontSize: 18),
+                            FadeAnimation(
+                                1.2,
+                                makeInput(
+                                  label: "Email",
+                                  obscureText: false,
+                                  onChanged: (text) {
+                                    email = text;
+
+                                    // print("Email = " + text);
+                                  },
+                                )),
+                            FadeAnimation(
+                                1.3,
+                                makeInput(
+                                  label: "Password",
+                                  obscureText: true,
+                                  onChanged: (text) {
+                                    password = text;
+                                    // print("Password = " + text);
+                                  },
+                                )),
+                          ],
+                        ),
+                      ),
+                      FadeAnimation(
+                          1.4,
+                          Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 40),
+                            child: Container(
+                              padding: EdgeInsets.only(top: 3, left: 3),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(50),
+                                  border: Border.all(color: ktextColor)),
+                              child: MaterialButton(
+                                minWidth: double.infinity,
+                                height: getProportionateScreenHeight(70, context),
+                                onPressed: () async{
+                                  validateAndSave();
+                                  if (validate == false) {
+                                    return null;
+                                  } else {
+                                    try{
+                                      final user = await _auth.signInWithEmailAndPassword(email: email, password: password);
+                                      if(user != null)
+                                        {
+                                          print("Able to Login");
+                                        }
+                                    }
+                                    catch(e){
+                                      showDialog(context: context,
+                                      builder: (BuildContext context){
+                                        return AlertDialog(title: Text(e.message,style: TextStyle(color: ktextColor),),actions: <Widget>[
+                                          RawMaterialButton(
+                                            onPressed: (){
+                                              Navigator.pop(context);
+                                            },
+                                            child: Text("try again",style: TextStyle(color: ktextColor,fontSize: getProportionateScreenWidth(13, context)),),
+
+
+                                          )
+                                        ],);
+                                      });
+
+                                    }
+
+
+                                    //      await FirebaseAuth.instance.signInWithEmailAndPassword(email: email, password: password)
+                                    //         .catchError((e) {print("Login Error ========="+e.toString());
+                                    //    Scaffold.of(context).showSnackBar(SnackBar( duration: const Duration(seconds: 3),
+                                    //      content: Text(e.message.toString(),style: TextStyle(color: Colors.red),),));
+                                    //         });
+                                    //
+                                    //
+                                    // if (FirebaseAuth.instance.currentUser != null) {
+                                    //   print("Able to Login");
+                                    //   print(FirebaseAuth.instance.currentUser.email.toString());
+                                    //   // Navigator.pushReplacement(
+                                    //   //     context,
+                                    //   //     MaterialPageRoute(
+                                    //   //         builder: (context) =>
+                                    //   //             home_screen()));
+                                    // }
+                                    //
+                                    //
+                                    // print(
+                                    //     "Login ------------ > email $email Password $password");
+                                  }
+                                },
+                                color: kMainColor,
+                                elevation: 0,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(50)),
+                                child: Text(
+                                  "Login",
+                                  style: TextStyle(
+                                      color: ktextColor,
+                                      fontWeight: FontWeight.w600,
+                                      fontSize: 18),
+                                ),
                               ),
                             ),
-                          ],
-                        ))
-                  ],
+                          )),
+                      FadeAnimation(
+                          1.5,
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Text("Don't have an account?"),
+                              GestureDetector(
+                                onTap: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => SignUpScreen()));
+                                },
+                                child: Text(
+                                  "Sign up",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w600, fontSize: 18),
+                                ),
+                              ),
+                            ],
+                          ))
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
